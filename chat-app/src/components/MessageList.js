3442e4cb7a9eb91f2db047dd61e5d61e0b7d0fd7
brizism <montielbriseida@gmail.com>
2018-08-22 12:59:21 -0400

    display list of messages

--- chat-app/src/components/MessageList.js
+++ chat-app/src/components/MessageList.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class MessageList extends React.Component {
+class MessageList extends Component {
     render() {
         return (
             <div className="message-list">
-                <div className="help-text">MessageList</div>
+                {this.props.messages.map((message, index) => {
+                  return (
+                    <div  key={index} className="message">
+                      <div className="message-username">{message.senderId}</div>
+                      <div className="message-text">{message.text}</div>
+                    </div>
+                  )
+                })}
             </div>
         )
     }
