036051c2e1dd0e804a172477ca1fe0d00ec1c0bb
brizism <montielbriseida@gmail.com>
2018-08-22 17:23:09 -0400

    listen to form input

--- chat-app/src/components/SendMessageForm.js
+++ chat-app/src/components/SendMessageForm.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
 class SendMessageForm extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            message: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state.message);
+    }
+
     render() {
         return (
-            <form className="send-message-form">
+            <form 
+                onSubmit={this.handleSubmit}
+                className="send-message-form">
                 <input
-                    placeholder="SendMessageForm"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type your message and hit ENTER"
                     type="text" />
             </form>
         )
