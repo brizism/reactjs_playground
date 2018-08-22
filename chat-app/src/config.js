4a3266e32dcbc59058242a4948cbaad1a39d42e8
brizism <montielbriseida@gmail.com>
2018-08-22 12:59:07 -0400

    connect to chatkit

---
+++ chat-app/src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/a659ccc1-6d34-48ae-a7c2-bfeca3fa8fb3/token";
+const instanceLocator = "v1:us1:a659ccc1-6d34-48ae-a7c2-bfeca3fa8fb3";
+
+export { tokenUrl, instanceLocator }
