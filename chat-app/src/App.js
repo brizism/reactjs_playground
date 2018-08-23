2bf4ab6738f3c316070ea39b7fd6cb2cffda7d69
brizism <montielbriseida@gmail.com>
2018-08-22 23:18:21 -0400

    fetch rooms

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component {
     constructor(){
       super()
       this.state = {
-        messages: []
+        messages: [],
+        joinableRooms: [],
+        joinedRooms: []
       }
     }
     
@@ -28,6 +30,16 @@ class App extends React.Component {
         chatManager.connect()
         .then(currentUser => {
           this.currentUser = currentUser
+
+          this.currentUser.getJoinableRooms()
+            .then(joinableRooms => {
+              this.setState({
+                joinableRooms,
+                joinedRooms: this.currentUser.rooms
+              })
+            })
+            .catch(err => console.log('error on joinableRooms: ', err))
+
           this.currentUser.subscribeToRoom({
                 roomId: 14472165,
                 hooks: {
@@ -39,6 +51,7 @@ class App extends React.Component {
                 }
             })
         })
+        .catch(err => console.log('error on connecting: ', err))
     }
 
     sendMessage = (text) => {
@@ -51,7 +64,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="app">
-                <RoomList />
+                <RoomList rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]} />
                 <MessageList messages={this.state.messages} />
                 <SendMessageForm  sendMessage={this.sendMessage} />
                 <NewRoomForm />
